72bff98736e94f4d5990b14cc40c76b57dc18366
EhsanMrf <developermrf@gmail.com>
2024-04-19 17:13:24 +0330

    init project and config ignoreGit add class , StrategySms

---
+++ RetryResilien/RetryResilien/SmsPipeLine.cs
@@ -0,0 +1,26 @@
+﻿using Polly;
+using Polly.Retry;
+
+namespace RetryResilience;
+
+public static class SmsPipeLine
+{
+    public const string KeyPipeLine = "SsmsPipeLine";
+
+    public static void StrategySms(int maxRetry)
+    {
+        var retryStrategyOptions = new RetryStrategyOptions<HttpResponseMessage>
+        {
+            MaxRetryAttempts = maxRetry,
+            ShouldHandle = new PredicateBuilder<HttpResponseMessage>()
+                .Handle<HttpRequestException>()
+                .HandleResult(response => response.StatusCode == System.Net.HttpStatusCode.InternalServerError),
+
+            Delay = TimeSpan.FromSeconds(1),
+            MaxDelay = TimeSpan.FromSeconds(15),
+            BackoffType = DelayBackoffType.Exponential,
+            UseJitter = true,
+        };
+
+    }
+}
